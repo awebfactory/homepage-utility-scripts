3b93aa9c40dac690168436d80008195f6d18104e
Victor Kane <victorkane@gmail.com>
2020-02-09 16:50:21 -0500

    feat(script): added convenient options format url, data, config

--- get-jwt.js
+++ get-jwt.js
@@ -1,11 +1,17 @@
 const axios = require('axios');
 
 const login = async () => {
-  console.log('hola');
   try {
-	  res = await axios.get('http://awebfactory.org:4004/users');
+	  const url = 'http://awebfactory.org:4004/users';
+		const data = {};
+		const config = {
+		  headers: {
+			  'Content-Type': 'application/json'
+      }
+		}
+	  res = await axios.get(url, data, config);
 		// console.log('res.data', res.data);
-		console.log('res', res);
+		console.log(res.status, res.statusText, "\n", res.data);
 	} catch (err) {
 	  console.error(err);
 	}
