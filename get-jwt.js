159a0fc013e6cbf74f210467d169c4c3905aca88
Victor Kane <victorkane@gmail.com>
2020-02-10 08:22:02 -0500

    feat(script): login to backend strapi server based on user and password in config file and obtain an auth token

--- get-jwt.js
+++ get-jwt.js
@@ -2,17 +2,22 @@ const axios = require('axios');
 require('dotenv').config();
 
 const login = async () => {
-  const api = 'users';
+  const api = '/auth/local';
   try {
-	  const url = `${process.env.API_SERVER}/${api}`
-		const data = {};
+	  const url = `${process.env.API_SERVER}${api}`;
+		const data = {
+      identifier: `${process.env.API_USER}`,
+      password: `${process.env.API_PASSWORD}`
+    };
+    console.log (url);
+    console.log (data);
 		const config = {
 		  headers: {
 			  'Content-Type': 'application/json'
       }
-		}
-	  res = await axios.get(url, data, config);
-		// console.log('res.data', res.data);
+		};
+    // POST
+	  res = await axios.post(url, data, config);
 		console.log(res.status, res.statusText, "\n", res.data);
 	} catch (err) {
 	  console.error(err);
