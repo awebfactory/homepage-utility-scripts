e4978c674ef5b4fca2c40810bfd152ba351b2228
Victor Kane <victorkane@gmail.com>
2020-02-10 13:12:27 -0500

    feat(get-api-request): accept GET api as command line parameter

---
+++ get-api-request.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const getApiReq = async (api) => {
+  try {
+	  const url = `${process.env.API_SERVER}${api}`
+		const config = {
+		  headers: {
+			  'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.API_JWT}` 
+      }
+		}
+    // console.log(config)
+	  res = await axios.get(url, config);
+		// console.log('res.data', res.data);
+		console.log(res.status, res.statusText, "\n", res.data);
+	} catch (err) {
+	  console.error(err.message);
+	}
+}
+
+// Example call: node get-api /tags
+const api = process.argv[2]
+
+getApiReq(api);
