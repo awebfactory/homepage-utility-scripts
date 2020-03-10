39ae8b2d14e493641d9fbd1bcdcd18df5c72c2b4
Victor Kane <victorkane@gmail.com>
2020-03-10 06:38:23 -0400

    feat(file structure): scripts in scripts dir, gitignore protected output (output) and post data (postit) dirs

---
+++ scripts/post-api-request.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const data = require('../postit/post-data.json');
+const api = process.argv[2];
+
+// console.log(data);
+
+// take each item in the array of json objects and process
+data.forEach( item => console.log("item: \n", item));
+
---
+++ scripts/show_data.js
@@ -0,0 +1,3 @@
+var data = require('../postit/post-data.json');
+
+data.forEach (item => console.log("item: \n", item));
