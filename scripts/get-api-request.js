18f5a443ce526d707d05f3a59936669a275386b9
Victor Kane <victorkane@gmail.com>
2020-03-10 07:40:56 -0400

    chore(prettier): Added prettier to vim, tested by running manually on one script

--- scripts/get-api-request.js
+++ scripts/get-api-request.js
@@ -1,26 +1,26 @@
-const axios = require('axios');
-require('dotenv').config();
+const axios = require('axios')
+require('dotenv').config()
 
-const getApiReq = async (api) => {
+const getApiReq = async api => {
   try {
-	  const url = `${process.env.API_SERVER}${api}`
-		const config = {
-		  headers: {
-			  'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.API_JWT}` 
-      }
-		}
+    const url = `${process.env.API_SERVER}${api}`
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.API_JWT}`,
+      },
+    }
     // console.log(config)
-	  res = await axios.get(url, config);
-		// console.log('res.data', res.data);
-		console.error(res.status, res.statusText, "\n");
-    console.log(JSON.stringify(res.data));
-	} catch (err) {
-	  console.error(err.message);
-	}
+    res = await axios.get(url, config)
+    // console.log('res.data', res.data);
+    console.error(res.status, res.statusText, '\n')
+    console.log(JSON.stringify(res.data))
+  } catch (err) {
+    console.error(err.message)
+  }
 }
 
 // Example call: node get-api /tags
 const api = process.argv[2]
 
-getApiReq(api);
+getApiReq(api)
