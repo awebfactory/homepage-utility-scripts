9ee275e69d40ee558a006d3063d96917705542ae
Victor Kane <victorkane@gmail.com>
2020-03-12 15:34:02 -0400

    feat(post): implemented posting of new content (2-3 tags) from file to Strapi backend

--- scripts/post-api-request.js
+++ scripts/post-api-request.js
@@ -6,5 +6,26 @@ const api = process.argv[2]
 
 // console.log(data);
 
+const postit = () => {
+  data.forEach(async item => {
+    // console.log('item: \n', item)
+    try {
+      const url = `${process.env.API_SERVER}${api}`
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.API_JWT}`,
+        },
+      }
+      // POST
+      res = await axios.post(url, item, config)
+      console.log(res.status, res.statusText, '\n', res.data)
+    } catch (err) {
+      console.error(err)
+    }
+  })
+}
+
 // take each item in the array of json objects and process
-data.forEach(item => console.log('item: \n', item))
+
+postit()
