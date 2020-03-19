080541d2e600efab1d0a06a261ea49e7b86f9d82
Victor Kane <victorkane@gmail.com>
2020-03-19 18:47:11 -0400

    feat(http): added needle based support for POST with cute short yearn script command

---
+++ needle-scripts/post-api-request.js
@@ -0,0 +1,25 @@
+const needle = require('needle')
+require('dotenv').config()
+
+const api = process.argv[2]
+const data = require(`../postit/post-${api}.json`)
+
+const postApiReq = (api, data) => {
+  data.forEach(async item => {
+    try {
+      const url = `${process.env.API_SERVER}/${api}`
+      const options = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.API_JWT}`,
+        },
+      }
+      const res = await needle('post', url, item, options)
+      console.log(res.body)
+    } catch (err) {
+      console.error(err.message)
+    }
+  })
+}
+
+postApiReq(api, data)
